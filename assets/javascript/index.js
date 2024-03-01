801109a8345f35d7e72953e362e39f6dd58d20fd
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-03-01 04:33:35 +0200

    feat(sign in): sign in now takes you to todo page

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,4 @@
-// when its clicked
 const Login = (event) => {
-    // get the values from username and pass
-    //   to get the values I have to call the elements and then get the value
     event.preventDefault();
     const correctName = "admin";
     const correctPass = "admin1";
@@ -9,9 +6,6 @@ const Login = (event) => {
     const password = document.getElementById("password").value;
     let hiddenMsg = document.getElementById("hiddenMsg");
     let hiddenLoading= document.getElementById("hidden-loading");
-    //   console.log(username);
-    //   console.log(password);
-    // check if the values match
     if (username == correctName && password == correctPass) {
       // if login success
       // go to todo page
@@ -20,11 +14,12 @@ const Login = (event) => {
       hiddenMsg.style.border= "1px solid green";
       hiddenMsg.style.backgroundColor= "#0080003d";
       hiddenMsg.innerHTML = "Logging you in!";
-      hiddenLoading.style.display = "block"
-      // get login from database login()
+      hiddenLoading.style.display = "block";
+      
+      //timeout the login to "gimmick for taking from db and seeing the gif"
       
       setTimeout(()=>{
-          window.location = "http://127.0.0.1:5500/todo-list-assignment/todo.html";
+          window.location = "http://127.0.0.1:5500/todo.html";
       },2000);
   
      
